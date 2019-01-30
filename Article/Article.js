808357b193865de82353ff24e7a32288e1256b4e
catslanderer <45492869+catslanderer@users.noreply.github.com>
2019-01-30 16:34:15 -0500

    Fixed expandButton so its text returns to 'expand' when the article is closed

--- Article/Article.js
+++ Article/Article.js
@@ -15,7 +15,9 @@ class Article {
   expandArticle() {
     // Using our reference to the domElement, toggle a class to expand or hide the article.
     this.domElement.classList.toggle("article-open");
-    this.expandButton.textContent = "close";
+    this.expandButton.textContent === "expand"
+      ? (this.expandButton.textContent = "close")
+      : (this.expandButton.textContent = "expand");
   }
 }
 
